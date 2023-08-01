ae613bedd6d1c7186dd6b8e1c9d357f8c68fa5f2
BelminKenjar <98585292+BelminKenjar@users.noreply.github.com>
2023-08-01 17:58:57 +0200

    Add files via upload

---
+++ Task2WinApp/Task2WinApp/LookupForm.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace Task2WinApp
+{
+    partial class LookupForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnLookup = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.txtLookupPath = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblObject = new System.Windows.Forms.Label();
+            this.err1 = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.err1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnLookup
+            // 
+            this.btnLookup.Location = new System.Drawing.Point(387, 12);
+            this.btnLookup.Name = "btnLookup";
+            this.btnLookup.Size = new System.Drawing.Size(134, 23);
+            this.btnLookup.TabIndex = 5;
+            this.btnLookup.Text = "Look up";
+            this.btnLookup.UseVisualStyleBackColor = true;
+            this.btnLookup.Click += new System.EventHandler(this.btnLookup_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblResult.Location = new System.Drawing.Point(12, 57);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(0, 15);
+            this.lblResult.TabIndex = 4;
+            // 
+            // txtLookupPath
+            // 
+            this.txtLookupPath.Location = new System.Drawing.Point(12, 12);
+            this.txtLookupPath.Name = "txtLookupPath";
+            this.txtLookupPath.PlaceholderText = "Enter your desired lookup path here";
+            this.txtLookupPath.Size = new System.Drawing.Size(369, 23);
+            this.txtLookupPath.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 15);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Result:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 96);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(99, 15);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Object to lookup:";
+            // 
+            // lblObject
+            // 
+            this.lblObject.AutoSize = true;
+            this.lblObject.Location = new System.Drawing.Point(12, 118);
+            this.lblObject.MaximumSize = new System.Drawing.Size(500, 0);
+            this.lblObject.Name = "lblObject";
+            this.lblObject.Size = new System.Drawing.Size(0, 15);
+            this.lblObject.TabIndex = 8;
+            // 
+            // err1
+            // 
+            this.err1.ContainerControl = this;
+            // 
+            // LookupForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(533, 201);
+            this.Controls.Add(this.lblObject);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnLookup);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.txtLookupPath);
+            this.Name = "LookupForm";
+            this.Text = "LookupForm";
+            this.Load += new System.EventHandler(this.LookupForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.err1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button btnLookup;
+        private Label lblResult;
+        private TextBox txtLookupPath;
+        private Label label1;
+        private Label label2;
+        private Label lblObject;
+        private ErrorProvider err1;
+    }
+}
---
+++ Task2WinApp/Task2WinApp/LookupForm.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task2WinApp
+{
+    public partial class LookupForm : Form
+    {
+        //Create example object for function testing
+        private object exampleObject = new
+        {
+            fruit = new
+            {
+                fruit1 = "Orange",
+                fruit2 = "Apple",
+            },
+             vegetables = new
+             {
+                 vegetables1 = "Tomato",
+                 vegetables2 = "Potato",
+             }
+        };
+        public LookupForm()
+        {
+            InitializeComponent();
+        }
+
+        private void LookupForm_Load(object sender, EventArgs e)
+        {
+            // Initial UI setup
+            lblResult.Text = "";
+            txtLookupPath.Text = "";
+            lblObject.Text = exampleObject.ToString();
+        }
+
+        private void btnLookup_Click(object sender, EventArgs e)
+        {
+            //set textbox value to a new variable
+            string lookupPath = txtLookupPath.Text.Trim();
+            //only execute function if there is text in the lookup textbox
+            if (lookupPath != "")
+            {
+                object value = ObjectUtils.Lookup(exampleObject, lookupPath);
+                //if my function returns null (returns null in case of errors) type out error message as result, otherwise type result 
+                lblResult.Text = value != null ? value.ToString() : "Value not found or invalid path";
+            }
+        }
+    }
+    public static class ObjectUtils
+    {
+        public static object Lookup(object obj, string path)
+        {
+            //a try catch block for error handling
+            try
+            {
+                //split textbox input into strings every time a dot is found
+                var keys = path.ToLower().Split('.');
+                var value = obj;
+
+                //for every string in keys try to find a matching value in our object
+                foreach (var key in keys)
+                {
+                    var type = value.GetType();
+                    var propertyInfo = type.GetProperty(key);
+
+                    if (propertyInfo == null)
+                    {
+                        throw new ArgumentException($"Property '{key}' not found in object");
+                    }
+
+                    value = propertyInfo.GetValue(value);
+                }
+
+                return value;
+            }
+            catch (Exception)
+            {
+                return null; // Return null in case of any errors
+            }
+        }
+    }
+}
